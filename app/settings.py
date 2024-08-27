48f3d78cb2e719c4765f2941a696ebf6c98022d6
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-08-27 15:11:39 -0300

    feat: adc nova funcionalidades

--- app/settings.py
+++ app/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -122,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- cars/models.py
+++ cars/models.py
@@ -18,7 +18,10 @@ class Car(models.Model):
         brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
         factory_year = models.IntegerField(blank=True, null=True)
         model_year = models.IntegerField(blank=True, null=True)
+        plate = models.CharField(max_length=200, blank=True, null=True)
         value = models.FloatField()
+        photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+
 
         def __str__(self):
                 return self.model
