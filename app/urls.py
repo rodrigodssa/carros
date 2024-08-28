96dd59b73c40b060532cad148ac13de6e5362fbc
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-08-27 21:51:31 -0300

    feat: adc nova funcao

--- app/urls.py
+++ app/urls.py
@@ -20,7 +20,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from cars.views import cars_view
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cars/', cars_view),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
