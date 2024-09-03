225f7a64366606e2394ab2f40780dec2b38702e8
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-09-02 21:40:38 -0300

    feat: add novas funcionalidades em views.py

--- cars/views.py
+++ cars/views.py
@@ -1,11 +1,18 @@
+from re import search
+
 from django.shortcuts import render
 from cars.models import Car
 
 
 def cars_view(request):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('model')
+    search = request.GET.get('search')
 
-    return render(request,
+    if search:
+        cars = Car.objects.filter(model__icontains=search)
+
+    return render(
+        request,
     'cars.html',
          {'cars': cars }
      )
