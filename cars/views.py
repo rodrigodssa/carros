58b95ca6bb591ccd76662ce9b418a7cc005bff83
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-09-15 19:34:28 -0300

    feat(views): add new_cars_view for creating new cars

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,7 @@
 from re import search
-
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cars.models import Car
-
+from cars.forms import CarForm
 
 def cars_view(request):
     cars = Car.objects.all().order_by('model')
@@ -11,8 +10,19 @@ def cars_view(request):
     if search:
         cars = Car.objects.filter(model__icontains=search)
 
-    return render(
-        request,
-    'cars.html',
-         {'cars': cars }
-     )
+    return render(request,'cars.html',{'cars': cars})
+
+
+
+def new_cars_view(request):
+    if request.method == 'POST':
+        new_car_form = CarForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+    else:
+        new_car_form = CarForm()
+
+    return render(request, "new_car.html", {'new_car_form': new_car_form})
+
+
