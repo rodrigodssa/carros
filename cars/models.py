63a9a7ccf3d45633b72f5b702041cd2d30783ec4
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-09-16 15:23:10 -0300

    style(base): ajustada a formatação e espaços no código

--- cars/models.py
+++ cars/models.py
@@ -9,9 +9,6 @@ class Brand(models.Model):
         def __str__(self):
                 return self.name
 
-
-
-
 class Car(models.Model):
         id = models.AutoField(primary_key=True)
         model = models.CharField(max_length=200)
@@ -19,9 +16,11 @@ class Car(models.Model):
         factory_year = models.IntegerField(blank=True, null=True)
         model_year = models.IntegerField(blank=True, null=True)
         plate = models.CharField(max_length=200, blank=True, null=True)
-        value = models.FloatField()
+        value = models.FloatField(blank=True, null=True)
         photo = models.ImageField(upload_to='cars/', blank=True, null=True)
 
 
         def __str__(self):
                 return self.model
+
+
