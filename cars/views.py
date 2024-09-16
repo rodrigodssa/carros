3b42863468aaa6f7a4d510d09b180e2ed94c4d3e
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-09-16 15:24:55 -0300

    refactor(views): alterado nome de CarModelForm em views.py

--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,7 @@
-from re import search
 from django.shortcuts import render, redirect
 from cars.models import Car
-from cars.forms import CarForm
+from cars.forms import CarModelForm
+
 
 def cars_view(request):
     cars = Car.objects.all().order_by('model')
@@ -16,12 +16,12 @@ def cars_view(request):
 
 def new_cars_view(request):
     if request.method == 'POST':
-        new_car_form = CarForm(request.POST, request.FILES)
+        new_car_form = CarModelForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
     else:
-        new_car_form = CarForm()
+        new_car_form = CarModelForm()
 
     return render(request, "new_car.html", {'new_car_form': new_car_form})
 
