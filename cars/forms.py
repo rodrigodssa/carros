4bf2331d8460ba2bcbc749126f7c093b78b80757
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-09-16 15:19:41 -0300

    refactor(forms): otimizada a função save no CarModelForm com menos linhas de código

--- cars/forms.py
+++ cars/forms.py
@@ -1,28 +1,20 @@
 from django import forms
-from django.forms import ModelChoiceField
-from cars.models import Brand, Car
+from cars.models import Car
 
 
-class CarForm(forms.Form):
-    model = forms.CharField(max_length=200)
-    brand = forms.ModelChoiceField(Brand.objects.all())
-    factory_year = forms.IntegerField()
-    model_year = forms.IntegerField()
-    plate = forms.CharField()
-    value = forms.FloatField()
-    photo = forms.ImageField()
+class CarModelForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = '__all__'
 
-    def save(self):
-        car: Car = Car(
-            model=self.cleaned_data['model'],
-            brand=self.cleaned_data['brand'],
-            factory_year=self.cleaned_data['factory_year'],
-            model_year=self.cleaned_data['model_year'],
-            plate=self.cleaned_data['plate'],
-            value=self.cleaned_data['value'],
-            photo=self.cleaned_data['photo'],
+    def clean_value(self):
+        value = self.cleaned_data.get('value')
+        if value < 20000:
+            self.add_error('value', 'Valor minimo do carro deve ser de R$20.000')
+        return value
 
-        )
-        car.save()
-        return car
-
+    def clean_factory_year(self):
+        factory_year = self.cleaned_data.get('factory_year')
+        if factory_year < 1975:
+            self.add_error('factory_year', 'Ano do Carro deve ser Maior que 1980')
+        return factory_year
