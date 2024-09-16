7366150c5d9a36225c9aaa7c49c0eef0e175c61e
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-09-16 16:23:22 -0300

    feat(auth): criada nova rota para registro de usuários

--- app/urls.py
+++ app/urls.py
@@ -1,18 +1,14 @@
-from django.contrib import admin
-from django.urls import path
-from django.contrib import admin
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+from accounts.views import register_view
 from cars.views import cars_view, new_cars_view
-
-
-
-
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/',register_view, name='register'),
     path('cars/', cars_view, name='cars_list'),
     path('new_car/', new_cars_view, name='new_car'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
