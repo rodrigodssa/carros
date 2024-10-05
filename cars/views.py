750169561a2902167a60a447a3f5b913d6d1865d
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-10-05 17:17:49 -0300

    feat: add ListView para exibição de carros

--- cars/views.py
+++ cars/views.py
@@ -1,28 +1,32 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
+from django.views.generic import ListView
 
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('model')
-    search = request.GET.get('search')
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
 
-    if search:
-        cars = Car.objects.filter(model__icontains=search)
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        if search:
+            cars = cars.filter(model__icontains=search)
+        return cars
 
-    return render(request,'cars.html',{'cars': cars})
 
+class NewCarView(View):
 
+    def get(self, request):
+        new_car_form = CarModelForm()
+        return render(request, "new_car.html", {'new_car_form': new_car_form})
 
-def new_cars_view(request):
-    if request.method == 'POST':
+    def post(self, request):
         new_car_form = CarModelForm(request.POST, request.FILES)
         if new_car_form.is_valid():
             new_car_form.save()
             return redirect('cars_list')
-    else:
-        new_car_form = CarModelForm()
-
-    return render(request, "new_car.html", {'new_car_form': new_car_form})
-
-
+        return render(request, "new_car.html", {'new_car_form': new_car_form})
