13432a270690b98c03835c8d600507c26fa95880
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-11-24 22:40:07 -0300

    fix(cars/template): handle missing photo in car details template

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from accounts.views import register_view, login_view, logout_view
-from cars.views import CarsListView, NewCarCreateView, CarDetailView
+from cars.views import CarsListView, NewCarCreateView, CarDetailView, CarUpdateView, CarDeleteView
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
@@ -13,4 +13,6 @@ urlpatterns = [
                   path('cars/', CarsListView.as_view(), name='cars_list'),
                   path('new_car/', NewCarCreateView.as_view(), name='new_car'),
                   path('car/<int:pk>/', CarDetailView.as_view(), name='car_detail'),
+                  path('car/<int:pk>/update/', CarUpdateView.as_view(), name='car_update'),
+                  path('car/<int:pk>/delete/', CarDeleteView.as_view(), name='car_delete')
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
